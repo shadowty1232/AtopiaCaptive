e0871d4c0d96350a8ebfee9b186031ad3626d78b
Shadowty1232 <tyl@null.net>
2020-08-16 20:45:57 +0100

    Added Team APIs

--- Captive/src/main/java/org/atopiamc/dev/captive/API/Criminals.java
+++ Captive/src/main/java/org/atopiamc/dev/captive/API/Criminals.java
@@ -4,12 +4,14 @@ import org.atopiamc.dev.captive.Main;
 import org.atopiamc.dev.captive.Utils;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 
 public class Criminals {
 
     public Location spawn;
     public World world;
 
+    private Main plugin;
     public Criminals(Main plugin) {
         world = plugin.getServer().getWorld(Utils.getString("Criminals.Spawn.World"));
         double x = plugin.getConfig().getDouble("Criminals.Spawn.X");
@@ -20,5 +22,15 @@ public class Criminals {
 
     public Location getCriminalsSpawn() {
         return spawn;
+    }
+
+    public void setCriminalSpawn(Player p) {
+        Location playerLoc = p.getLocation();
+        double x = playerLoc.getX();
+        double y = playerLoc.getY();
+        double z = playerLoc.getZ();
+        plugin.getConfig().set("Criminals.Spawn.X", x);
+        plugin.getConfig().set("Criminals.Spawn.Y", y);
+        plugin.getConfig().set("Criminals.Spawn.Z", z);
     }
 }
